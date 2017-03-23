a13d44573b3a6e6242e6c7c7c2bb8fd9c17ec87f
游泽渠 <287124371@qq.com>
2017-03-23 22:35:24 +0800

    把获取活动和反馈获取以及提交的接口都写好了

---
+++ app/Http/Controllers/Feedback.php
@@ -0,0 +1,62 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Traits\AuthOperationWithRequest;
+use App\Feedback as FeedbackModel;
+
+class Feedback extends Controller
+{
+    use AuthOperationWithRequest;
+
+    protected $request;
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    public function scan()
+    {
+        $index = $this->request->input('index');
+        $offset = ($index - 1) * 30;
+        $feedbacks = FeedbackModel::with('FeedbackResponse')->where('user_id', '=', $this->getUserId())
+            ->orderBy('id', 'desc')
+            ->offset($offset)
+            ->limit(30)
+            ->get();
+        return response()->json($this->makeScanMessageBag($feedbacks));
+    }
+
+    public function handIn()
+    {
+        $content = $this->request->input('content');
+        FeedbackModel::create(array(
+            'content' => $content,
+            'created_at' => time(),
+            'user_id' => $this->getUserId()
+        ));
+        return response()->json(array(
+            'state' => true
+        ));
+    }
+
+    protected function makeScanMessageBag($feedbacks)
+    {
+        if (! count($feedbacks)) {
+            return array(
+                'state' => false
+            );
+        }
+        $message_bag = array(
+            'state'
+        );
+        foreach ($feedbacks as $key => $feedback) {
+            $message_bag['feedback'][$key]["feedbackCreateTime"] = $feedback->created_at->timestamp;
+            $message_bag['feedback'][$key]["feedbackContent"] = $feedback->content;
+            $message_bag['feedback'][$key]["respondCreateTime"] = $feedback->FeedbackResponse->created_at->timestamp;
+            $message_bag['feedback'][$key]["respondContent"] = $feedback->FeedbackResponse->content;
+        }
+        return $message_bag;
+    }
+}
